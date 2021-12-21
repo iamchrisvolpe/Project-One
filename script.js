950f026431dd1c0d6a8c5c09da65fc9d5d5355dd
Christopher Volpe II <iamchrisvolpe>
2021-12-21 17:30:03 -0500

    oved the starting dierection under identifyers so JS would work, and created a way to change direction of snake

--- script.js
+++ script.js
@@ -22,9 +22,23 @@ const  createFood  = () => {
     gameBoardPixels[currentFoodPostion].classList.add("food");
 };
 
-let  snakeCurrentDirection  =  RIGHT_DIR;
 
 const  LEFT_DIR  =  37;
 const  UP_DIR  =  38;
 const  RIGHT_DIR  =  39;
 const  DOWN_DIR  =  40;
+
+let  snakeCurrentDirection  =  RIGHT_DIR;
+
+const  changeDirection  =  newDirectionCode  => {
+    if (newDirectionCode  ==  snakeCurrentDirection) return;
+    if (newDirectionCode  ==  LEFT_DIR  &&  snakeCurrentDirection  != RIGHT_DIR) {
+        snakeCurrentDirection  =  newDirectionCode;
+    } else  if (newDirectionCode  ==  UP_DIR  &&  snakeCurrentDirection  !=  DOWN_DIR) {
+        snakeCurrentDirection  =  newDirectionCode;
+    } else  if (newDirectionCode  ==  RIGHT_DIR  && snakeCurrentDirection  !=  LEFT_DIR) {
+        snakeCurrentDirection  =  newDirectionCode;
+    } else  if (newDirectionCode  ==  DOWN_DIR  &&  snakeCurrentDirection  !=  UP_DIR) {
+        snakeCurrentDirection  =  newDirectionCode;
+    }
+};
