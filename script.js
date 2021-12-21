52c71520ef53d32a7974b2ed8f3c6b41e8c7a18b
Christopher Volpe II <iamchrisvolpe>
2021-12-21 17:20:25 -0500

    added game container to populated with element of pixels

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const GAME_PIXEL_COUNT = 40;
+const SQUARE_OF_GAME_PIXEL_COUNT = Math.pow(GAME_PIXEL_COUNT, 2);
+
+let totalFoodAte = 0;
+let totalDistanceTravelled = 0;
+
+const  gameContainer  =  document.getElementById("gameContainer");
+const  createGameBoardPixels  = () => {
+    for (let  i  =  1; i  <=  1600; ++i) {
+        gameContainer.innerHTML  =  `${gameContainer.innerHTML} <div class="gameBoardPixel" id="pixel${i}"></div>`;
+    }
+};
+
+const  gameBoardPixels  =  document.getElementsByClassName("gameBoardPixel");
+
